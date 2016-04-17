dccdf129aeb9ee0da12232ce2e837cbab4f54e86
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2016-04-17 13:18:42 +0200

    Removing repeated and unneeded code and change from rgb to hex

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,4 @@ jQuery(document).ready(function($) {
   var slider = $('.default-slider').unslider({
     arrows: false
   });
-  slider.on('unslider.change', function(event, index, slide) {
-    current = $('#preview').css('left');
-    current = 617+20*index;
-    current = current+'px';
-    $('#preview').css('left',current);
-  });
 });
